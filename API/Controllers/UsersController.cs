4e09cf7da09ce6e71e3d01e7cb9591002c72ffa3
Domenico Caronte <domenico.caronte@consultant.aruba.it>
2024-04-30 14:33:34 +0200

    Sezione 2 del corso terminata

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] // NOTA mettendo controller tra quadre corrisponde a api/users (prende il nome della class senza Controller)
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")] //corrisponde ad esempio a /api/users/2
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+
+        return user;
+    }
+}
+
